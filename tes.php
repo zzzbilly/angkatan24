135daf5497c76220f32e200c33859979ea7898b8
zzz_billy <89883199+zzzbilly@users.noreply.github.com>
2024-08-23 15:33:49 +0700

    Add files via upload

---
+++ tes.php
@@ -0,0 +1,15 @@
+<?php
+header("refresh:1; url=http://angkatanbroed24.unaux.com/")
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Angkatan 24 BROED</title>
+</head>
+<body>
+    
+</body>
+</html>
