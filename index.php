3cf500da82d228886ef45963e7289e02661ce07b
zzz_billy <89883199+zzzbilly@users.noreply.github.com>
2024-08-23 16:07:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
-<?php
-header("refresh:1; url=http://angkatanbroed24.unaux.com/")
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Angkatan 24 BROED</title>
+    <title>Redirecting...</title>
+    <?php
+    header("refresh:1; url=http://angkatanbroed24.unaux.com/");
+    ?>
 </head>
 <body>
-    
+    <p>Redirecting to <a href="http://angkatanbroed24.unaux.com/">http://angkatanbroed24.unaux.com/</a> in 1 second...</p>
 </body>
 </html>
